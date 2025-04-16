b79a6fd9ae44eb0ae1b535806ef24a820bbd49f3
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-04-16 17:31:37 +0100

    creation des composante

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbareComponent } from './navbare/navbare.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { QuestionComponent } from './question/question.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbareComponent,AccueilComponent,QuestionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
